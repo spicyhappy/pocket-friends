7aa37bccf6c7a3134f259d0b3a22c368e3be21a2
xinsq <mail@xin-squared.com>
2013-03-12 10:46:26 -0400

    Add canvas and geolocation

--- js/basic.js
+++ js/basic.js
@@ -1,44 +1,42 @@
-// (function() {
-
-//   console.log("working");
-//   var x=document.getElementById("demo");
-
-//    function showPosition(position)
-//      {
-//       console.log("hihi");
-//      x.innerHTML="Latitude: " + position.coords.latitude + 
-//      "<br>Longitude: " + position.coords.longitude; 
-//      }
-
-//  if (navigator.geolocation) {
-
-//       console.log("we have geolocation");
-//     navigator.geolocation.getCurrentPosition(showPosition);
-//       console.log("function returned");
-
-//   }
-
-//   else{
-//     x.innerHTML="Geolocation is not supported by this browser.";
-//   }
 
 
-// })();
 
-var x=document.getElementById("demo");
-function getLocation()
-  {
-  if (navigator.geolocation)
-    {
+
+
+
+
+$(document).ready(function() {
+
+  // Store variables
+  var canvas = $("#gameCanvas");
+  var context = canvas.get(0).getContext("2d");
+  var x = canvas.width();
+  var y = canvas.height();
+
+  // Set defaults
+  context.font = "18px san-serif";
+  context.fillStyle = "rgb(255,255,255)";
+  context.fillRect(0, 0, x, y);
+  context.fillStyle = "rgb(0,0,0)";
+
+  // Set latitude and longitude
+  function showPosition(position) {
+    latitude  =  position.coords.latitude;
+    longitude =  position.coords.longitude;
+
+    context.fillText("Latitude: "+latitude+". Longitude: "+longitude, 40, y/2);
+
+    console.log(latitude);
+    console.log(longitude);
+  }
+
+  // Detect for geolocation access
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
-    }
-  else{x.innerHTML="Geolocation is not supported by this browser.";}
-  }
-function showPosition(position)
-  {
-  x.innerHTML="Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;  
   }
 
-  (function(){getLocation()
-  })();
+  else {
+    context.fillText("Pocket friends does not have geolocation access.", x/2, y/2);
+  }
+
+});
